9f587cb6fd77c66a1f19f277b588dddfd596a8af
TrentKlinge <82176438+TrentKlinge@users.noreply.github.com>
2021-05-21 21:38:34 -0700

    Update js.js

--- js.js
+++ js.js
@@ -41,6 +41,37 @@ rLine +="</p>";
 document.getElementById("upLeft").innerHTML = rLine;
 }
 
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 1;i--){
+rLine +="<p>";
+  
+//fill in left side of every row with sapces
+for (x=1; x<=pHeight-i;x++){
+  //rLine += "-";
+  rLine +="<span class='space'>" + pSymbol +"</span>";
+}
+
+//Create each line on the Rhombus
+for(j=x;j<=pHeight;j++){
+
+//Is the position even or odd so we change the color
+if (j%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("downLeft").innerHTML = rLine;
+}
+
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
 var rLine ="";
 for (i=0;i<pHeight;i++){
